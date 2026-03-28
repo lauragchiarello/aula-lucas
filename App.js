6baf08c695b1f108a4fd62213a3e099e5201dbfc
LauraChiarello <laura_chiarello@live.com>
2026-03-27 22:26:01 -0300

    adição

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState, useRef} from "react"
+import { StyleSheet, View, Text } from 'react-native'
+import MapView,{Marker} from "react-native-maps"
+import * as Location from 'expo-location'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  
+  const[location,setLocation] = useState(null)
+  const[errorMsg, setErrorMsg] = useState(null)
+  const mapRef = useRef(null)
+
+  useEffect(()=>{
+    (async () => {
+
+      let {status} = await Location.requestForegroundPermissionsAsync()
+      if(status !== 'granted'){
+        setErrorMsg('A permissão para acessar a localizaçao foi negada')
+        return
+      }
+      const subscription = await Location.watchPositionAsync({
+        accuracy: Location.Accuracy.High,
+        timeInterval:1000,
+        distanceInterval:1
+      },
+      (newLocation) =>{
+        const{latitude, longitude} = newLocation.coords
+        setLocation({latitude, longitude})
+      }
+      )
+
+    })()
+  },[])
+  
+  return(
+    <View>
+
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
